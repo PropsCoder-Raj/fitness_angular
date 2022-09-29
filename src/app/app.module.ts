e93f4f876ff4f22bce82a0f6106427b42f9915d7
vaishnavi.ingle@indicchain.com <rohit.pandit@indicchain.com>
2022-09-29 10:40:28 +0530

    Create Auth Guard and Service using google auth provider. Integrate module and service in app module, component file.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { MySessionComponent } from './pages/my-session/my-session.component';
 import { MyPaymentHistoryComponent } from './pages/my-payment-history/my-payment-history.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { CommonModule } from '@angular/common';
+import { AuthService } from './services/auth.service';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 
 @NgModule({
   declarations: [
@@ -20,9 +24,11 @@ import { CommonModule } from '@angular/common';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CommonModule
+    CommonModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
